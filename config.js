b94609074d196f31b43daa9a294d2145e32a7aa6
elahacktr <110237735+elahacktr@users.noreply.github.com>
2022-11-03 19:56:23 +0300

    eloxy
    eloxy

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: "MTAzNzc2NjA4MDc2MTk1NDMxNA.GvbkpL.gEs7PV3B7F--_VMkR1qnq3Q8xioYZ28lULySsU", 
+    prefix: "a!",
+    rolspam: "@everyone",
+    kanalspam: "879822931339903077",
+    spam: "ELOXY ANANIZI SİKER"
+}
+
